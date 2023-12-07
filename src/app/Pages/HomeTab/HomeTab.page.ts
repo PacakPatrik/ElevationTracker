f88f2b8fc0fb121186afde34e899ade093274bc8
pacak <p_pacak@utb.cz>
2023-12-07 20:06:23 +0100

    Working elevation updating and on/off tracking

--- src/app/Pages/HomeTab/HomeTab.page.ts
+++ src/app/Pages/HomeTab/HomeTab.page.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { LocationService } from "../../services/location-service/location.service";
 import { ElevationService } from "../../services/elevation-service/elevation.service";
-import {Observable, of, Subject} from "rxjs";
+import {interval, Observable, of, Subject, timer} from "rxjs";
 import { switchMap, takeUntil } from 'rxjs/operators';
 import { Elevation } from "../../model/elevation.model";
 
@@ -12,28 +12,58 @@ import { Elevation } from "../../model/elevation.model";
 })
 export class HomeTabPage {
 
-    private destroy$ = new Subject<void>();
-    elevation$: Observable<Elevation> | undefined;
-
+  private destroy$ = new Subject<void>();
+  elevation$: Observable<Elevation> | undefined;
+  private timerSubject = new Subject<number>();
+  public timer$: Observable<number> = this.timerSubject.asObservable();
+  private timer: any;
+  public time: number = 0;
+  private isTracking : boolean;
+  buttonText: string = 'Start tracking';
+  buttonColor: string = 'success';
     constructor(
         private elevationService: ElevationService,
         private locationService: LocationService
-    ) {}
+    ) {
+
+      this.isTracking=false;
+    }
+
+
+  toggleTracking() {
+    if (!this.isTracking) {
+      this.startTracking();
+    } else {
+      this.stopTracking();
+    }
+  }
 
     startTracking() {
-        this.destroy$.next();
-        this.destroy$.complete();
+      this.isTracking =true;
+      this.destroy$.next();
+      this.destroy$.complete();
 
-        this.locationService.getLocation().then(coordinates => {
-            this.elevation$ = this.elevationService.getByGeo$(coordinates.coords.latitude, coordinates.coords.longitude)
-                .pipe(
-                    takeUntil(this.destroy$)
-                );
+      this.locationService.getLocation().then(coordinates => {
+        this.elevation$ = this.elevationService.getByGeo$(coordinates.coords.latitude, coordinates.coords.longitude)
+          .pipe(
+            takeUntil(this.destroy$)
+          );
 
-            this.elevation$.subscribe(data => {
-                console.log(data);
-            });
+        this.elevation$.subscribe(data => {
+          console.log(data);
         });
+      });
+
+
+      this.startTimer();
+      this.buttonText = 'Stop tracking';
+      this.buttonColor = 'danger';
+    }
+    stopTracking(){
+      this.isTracking =false;
+      this.stopTimer();
+      this.buttonText = 'Start tracking';
+      this.buttonColor = 'success';
     }
 
     ngOnDestroy() {
@@ -41,4 +71,31 @@ export class HomeTabPage {
         this.destroy$.next();
         this.destroy$.complete();
     }
+
+  startTimer() {
+    this.timer = setInterval(() => {
+      this.time++;
+
+
+      this.locationService.getLocation().then(coordinates => {
+        this.elevation$ = this.elevationService.getByGeo$(coordinates.coords.latitude, coordinates.coords.longitude)
+          .pipe(
+            takeUntil(this.destroy$)
+          );
+
+        this.elevation$.subscribe(data => {
+          console.log(data);
+        });
+      });
+
+
+    }, 10000);
+  }
+
+  stopTimer() {
+    clearInterval(this.timer);
+    this.time = 0;
+    this.timerSubject.next(this.time);
+
+  }
 }
